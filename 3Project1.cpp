9a8ef40c819b44df58497e40feb7d425211a7281
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-03-27 00:03:25 +0600

    Add files via upload

---
+++ 3Project1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+std::string revertBoolName(bool value)
+{
+	if (value == false)
+		return "false";
+	else return "true ";
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	//Выполнение задания 1
+	std::cout << "Выполнение задания 1 \n\n";
+
+	std::vector<bool> a1 = { true, false, true, false };
+	std::vector<bool> b1 = { true, true, false, false };
+
+	std::cout << "Оператор: || \n";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << revertBoolName(a1.at(i)) << " ";
+		std::cout << revertBoolName(b1.at(i)) << " ";
+		std::cout << revertBoolName(a1.at(i) || b1.at(i));
+		std::cout << "\n";
+	}
+
+	std::cout << "\nОператор: && \n";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << revertBoolName(a1.at(i)) << " ";
+		std::cout << revertBoolName(b1.at(i)) << " ";
+		std::cout << revertBoolName((a1.at(i) && b1.at(i)));
+		std::cout << "\n";
+	}
+}
---
+++ 3Project2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	//Выполнение задания 2
+	std::cout << "\nВыполнение задания 2 \n\n";
+
+	int number1 = 0;
+	int number2 = 0;
+	int number3 = 0;
+
+	std::cout << "Введите первое число:";
+	std::cin >> number1;
+	std::cout << "Введите второе число:";
+	std::cin >> number2;
+	std::cout << "Введите третье число:";
+	std::cin >> number3;
+
+	int maxNumber = number1 > number2
+		? number1 > number3
+		? number1 : number3
+		: number2 > number3
+		? number2 : number3;
+
+	int minNumber = number1 < number2
+		? number1 < number3
+		? number1 : number3
+		: number2 < number3
+		? number2 : number3;
+
+	int centerNumber = (number1 != minNumber && number1 != maxNumber) ? number1 :
+		(number2 != minNumber && number2 != maxNumber) ? number2 : number3;
+
+	std::cout << "Результат: ";
+	std::cout << maxNumber << " ";
+	std::cout << centerNumber << " ";
+	std::cout << minNumber << " \n";
+}
